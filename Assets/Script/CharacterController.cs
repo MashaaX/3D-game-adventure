651e3143e8b88acaf680109340a7e27bf0aa101b
Kinanti Permata Putri <kinanti.permat@gmail.com>
2019-07-28 10:32:20 +0700

    Bisa Meloncat Tetapi Langsung Jatuh Kebawah

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float fowardVel = 12;
+        public float rotateVel = 100;
+        public float jumpVel = 25;
+        public float distToGrounded = 0.1f;
+        public LayerMask ground;
+    }
+
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downAccel = 0.75f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;
+        public string FOWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
+
+    public MoveSettings moveSetting = new MoveSettings();
+    public PhysSettings physSetting = new PhysSettings();
+    public InputSettings inputSetting = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float fowardInput, turnInput, jumpInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distToGrounded, moveSetting.ground);
+    }
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The Character Needs a Rigidbody");
+
+        fowardInput = turnInput = jumpInput = 0;
+    }
+
+    void GetInput()
+    {
+        fowardInput = Input.GetAxis(inputSetting.FOWARD_AXIS); //interpolated
+        turnInput = Input.GetAxis(inputSetting.TURN_AXIS); //interpolated
+        jumpInput = Input.GetAxisRaw(inputSetting.JUMP_AXIS); //non=interpolated
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+
+        rBody.velocity = transform.TransformDirection(velocity);
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(fowardInput) > inputSetting.inputDelay)
+        {
+            //move
+            velocity.z = moveSetting.fowardVel * fowardInput;
+        }
+        else
+        {
+            //zero velocity
+            velocity.z = 0;
+        }
+
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputSetting.inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSetting.rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+
+    void Jump()
+    {
+        if(jumpInput > 0 && Grounded())
+        {
+            //jump
+            velocity.y = moveSetting.jumpVel;
+        }
+        else if (jumpInput == 0 && Grounded())
+        {
+            //zero out our velocity.y
+            velocity.y = 0;
+        }
+        else
+        {
+            //decrease velocity.y
+            velocity.y -= physSetting.downAccel;
+        }
+    }
+}
