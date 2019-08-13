e881a020b2779992ae502339dd4f2a7851b1f680
Kinanti Permata Putri <kinanti.permat@gmail.com>
2019-08-13 19:01:35 +0700

    Player Attack

---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject Hand;
+    public Weapon myWeapon;
+    Animator handAnim;
+
+    private float attackTimer;
+    
+    void Start()
+    {
+        handAnim = Hand.GetComponent<Animator>();
+        myWeapon = Hand.GetComponentInChildren<Weapon>();
+    }
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+        if (Input.GetMouseButton(0) && attackTimer >= myWeapon.attackCoolDown)
+        {
+            DoAttack();
+        }
+
+        //Attack Animation
+        if (Input.GetMouseButton(0))
+        {
+            handAnim.SetTrigger("attack");
+        }
+    }
+
+    private void DoAttack()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, myWeapon.attackRange))
+        {
+            if(hit.collider.tag == "Enemy")
+            {
+                EnemyHealth ehealth = hit.collider.GetComponent<EnemyHealth>();
+                ehealth.TakeDamage(myWeapon.attackDamage);
+            }
+        }
+    }
+}
