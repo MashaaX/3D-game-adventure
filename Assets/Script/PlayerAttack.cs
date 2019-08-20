b48fb8e62f8dd6d2c318bfeb8f2a248641fbb9bd
m. nindra zaka <mnindrazaka@gmail.com>
2019-08-20 22:30:57 +0700

    fix attack animation and spawn enemy random position if attacked

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    Animator animator;
-    
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
+  Animator animator;
 
-    void Update()
-    {
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+  }
 
-        //Attack Animation
-        if (Input.GetMouseButton(0))
-        {
-            animator.SetTrigger("attack");
-        }
+  void Update()
+  {
+
+    //Attack Animation
+    if (Input.GetMouseButtonDown(0))
+    {
+      animator.SetTrigger("attack");
     }
+  }
+
+  private bool isAttacking()
+  {
+    return animator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Enemy") && isAttacking())
+    {
+      Destroy(other.gameObject);
+    }
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    if (other.CompareTag("Enemy") && isAttacking())
+    {
+      other.gameObject.transform.position = new Vector3(
+        Random.Range(5, 450),
+        0.84f,
+        Random.Range(5, 450)
+      );
+    }
+  }
 }
